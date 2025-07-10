506e47e03f511142b6cf49b1b1e67e6b27b27483
ÖMER FARUK ÖZÇELİK <148583343+ofarukozcelik@users.noreply.github.com>
2025-07-10 19:54:30 +0300

    Update firebaseConfig.ts

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDsYAw0phJBXY2eU5XlJA3Qv_wTB8UyJks',
-  authDomain: 'trendyol-230fe.firebaseapp.com',
-  projectId: 'trendyol-230fe',
-  storageBucket: 'trendyol-230fe.appspot.com',
-  messagingSenderId: '970485599062',
-  appId: '1:970485599062:ios:ec0a6740d97b79af12a4f7',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 const app = initializeApp(firebaseConfig);
